c5d0795d30ae6d300ae6cfb77808089abeff1087
kali <Guille@Guillermos-MacBook-Air.local>
2016-05-25 15:31:22 +0200

    New icon added

--- Scrollme/ViewController.swift
+++ Scrollme/ViewController.swift
@@ -10,16 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
+        
+        let v1 : View1 = View1(nibName: "View1", bundle: nil)
+        
+        let v2 : View2 = View2(nibName: "View2", bundle: nil)
+        
+        self.addChildViewController(v1)
+        self.scrollView.addSubview(v1.view)
+        v1.didMoveToParentViewController(self)
+        
+        self.addChildViewController(v2)
+        self.scrollView.addSubview(v2.view)
+        v2.didMoveToParentViewController(self)
+        
+        var v2Frame : CGRect = v2.view.frame
+        v2Frame.origin.x = self.view.frame.width
+        v2.view.frame = v2Frame
+        
+        self.scrollView.contentSize = CGSizeMake(self.view.frame.width * 2, self.view.frame.size.height)
+
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
-
+   
 }
-
---
+++ View2.swift
@@ -0,0 +1,82 @@
+//
+//  View2.swift
+//  Scrollme
+//
+//  Created by Guillermo Garcia on 5/23/16.
+//  Copyright © 2016 Vinter. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+
+class View2: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    var captureSession : AVCaptureSession?
+    var stillImageOutput : AVCaptureStillImageOutput?
+    var previewLayer : AVCaptureVideoPreviewLayer?
+    
+    
+    @IBOutlet var cameraView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        previewLayer?.frame = cameraView.bounds
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        captureSession = AVCaptureSession()
+        captureSession?.sessionPreset = AVCaptureSessionPreset1920x1080
+        
+        let backCamera = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        
+        var error : NSError?
+        var input: AVCaptureDeviceInput!
+        do {
+            input = try AVCaptureDeviceInput(device: backCamera)
+        } catch let error1 as NSError {
+            error = error1
+            input = nil
+        }
+        
+        if (error == nil && captureSession?.canAddInput(input) != nil){
+            
+            captureSession?.addInput(input)
+            
+            stillImageOutput = AVCaptureStillImageOutput()
+            stillImageOutput?.outputSettings = [AVVideoCodecKey : AVVideoCodecJPEG]
+            
+            if (captureSession?.canAddOutput(stillImageOutput) != nil){
+                captureSession?.addOutput(stillImageOutput)
+                
+                previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+                previewLayer?.videoGravity = AVLayerVideoGravityResizeAspect
+                previewLayer?.connection.videoOrientation = AVCaptureVideoOrientation.Portrait
+                cameraView.layer.addSublayer(previewLayer!)
+                captureSession?.startRunning()
+                
+            }
+            
+            
+        }
+        
+        
+    }
+    
+  
+    
+}
